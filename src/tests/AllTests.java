ecbeb86208ceba5142396279acdf0691c74479b7
Sadman Sakib <sadmansakib123450@gmail.com>
2024-10-18 22:56:48 +0200

    test suit added and exception added

---
+++ src/tests/AllTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+// Import the new JUnit 5 annotations
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SelectClasses;
+
+//I have Use the @Suite annotation to indicate this is a test suite
+@Suite
+// Use @SelectClasses to specify the test classes to include
+@SelectClasses({
+    EnergySimulatorTest.class,
+    FileReaderUtilTest.class,
+    LogManagerTest.class,
+    SimpleStringSearchTest.class,
+    RegexSearchTest.class
+})
+public class AllTests {
+    // This class remains empty; it's used only as a holder for the above annotations
+}
