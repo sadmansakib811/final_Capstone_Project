281d59ae931dc62727decb469834eeea7a3dfb86
FarhanaShaheed <farhana15-734@diu.edu.bd>
2024-10-27 00:05:28 +0200

    Synchronized method to discharge the battery

--- src/energymanagement/Battery.java
+++ src/energymanagement/Battery.java
@@ -24,6 +24,22 @@ public class Battery {
         System.out.println(source + " charged " + amount + " units. Current charge: " + currentCharge);
         notifyAll();
     }
+    
+ // Synchronized method to discharge the battery
+    public synchronized void discharge(int amount, String consumer) {
+        while (currentCharge - amount < 0) {
+            // Prevent over-discharging
+            try {
+                System.out.println(consumer + " waiting to discharge. Battery empty.");
+                wait(); // Wait until there's enough charge
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        currentCharge -= amount;
+        System.out.println(consumer + " discharged " + amount + " units. Current charge: " + currentCharge);
+        notifyAll(); // Notify other threads that may be waiting
+    }
 
    
 
